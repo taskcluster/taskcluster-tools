69a64f9e1397ee42b09e23f692286b794be00c10
John Ford <john@johnford.info>
2015-04-08 16:10:47 +0200

    List existing workerTypes

--- aws-provisioner/awsprovisioner.jsx
+++ aws-provisioner/awsprovisioner.jsx
@@ -6,7 +6,81 @@ var taskcluster     = require('taskcluster-client');
 var _               = require('lodash');
 var format          = require('../lib/format');
 
-/** Generic Index Browser with a custom entryView */
+/** 
+ * We are doing this without the mixin for now because the client needs
+ * to be created in a non-standard way.  This shouldn't be done this way
+ * in production...
+ */
+/** BEGIN SUPER HACK */
+var request = new XMLHttpRequest();
+//request.open('GET', 'https://taskcluster-aws-provisioner2.herokuapp.com/v1/api-reference', false);
+request.open('GET', 'http://localhost:5556/v1/api-reference', false);
+request.send(null);
+if (request.status === 200) {
+  var reftxt = request.responseText;
+  try {
+    var reference = JSON.parse(reftxt);
+    console.dir(reference);
+  } catch(e) {
+    console.log(e, e.stack);
+    alert('Uh-oh, error: ' + e);
+  }
+} else {
+  alert('Uh-oh, failed to load API reference');
+}
+
+var AwsProvisionerClient = taskcluster.createClient(reference);
+/** END SUPER HACK */
+
+// Questions:
+//  1. Should I create a client for each react class or share the parent classes?
+//  2. Why does the aws-provisioner not allow cross-origin requests?
+
+
+var WorkerTypeTable = React.createClass({
+  mixins: [
+    utils.createTaskClusterMixin({
+      clients: {
+        awsProvisioner: AwsProvisionerClient,
+      }
+    }),
+  ],
+
+  getInitialState: function() {
+    return {
+      workerTypes: [],
+      workerTypesLoaded: true,
+      workerTypesError: undefined,
+      current: '',
+    };
+  },
+
+  load: function() {
+    return {
+      workerTypes: this.awsProvisioner.listWorkerTypes(),
+    };
+  },
+  
+  render: function() {
+    return (
+      <bs.Table striped bordered condensed hover>
+        <thead>
+          <tr>
+            <th>Worker Type Name</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+          this.renderWaitFor('workerTypes') || this.state.workerTypes.map(function(name) {
+            return (<tr><td>{name}</td></tr>);
+          })
+        }
+        </tbody>
+      </bs.Table>
+    );
+  },
+});
+
 var AwsProvisioner = React.createClass({
   mixins: [
     // Calls load()
@@ -14,9 +88,6 @@ var AwsProvisioner = React.createClass({
       clients: {
         awsProvisioner:          taskcluster.Index
       },
-      // Reload when state.namespace changes, ignore credentials changes
-      reloadOnKeys:           ['workerType'],
-      reloadOnLogin:          true
     }),
   ],
 
@@ -25,7 +96,7 @@ var AwsProvisioner = React.createClass({
 
   getInitialState: function() {
     return {
-      workerType:       "",
+      workerType: "",
     };
   },
 
@@ -36,7 +107,7 @@ var AwsProvisioner = React.createClass({
 
   render: function() {
     return (
-        <p>Hello</p>
+        <WorkerTypeTable />
     );
   },
 
