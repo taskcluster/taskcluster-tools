276df0088ca423c787c2f66b8ce8305f86477c9c
John Ford <john@johnford.info>
2015-04-08 16:44:57 +0200

    Basic framework for loading stats

--- aws-provisioner/awsprovisioner.jsx
+++ aws-provisioner/awsprovisioner.jsx
@@ -6,6 +6,9 @@ var taskcluster     = require('taskcluster-client');
 var _               = require('lodash');
 var format          = require('../lib/format');
 
+// Should this be allowed to be set by user?
+var provisionerId = 'aws-provisioner2-dev';
+
 /** 
  * We are doing this without the mixin for now because the client needs
  * to be created in a non-standard way.  This shouldn't be done this way
@@ -72,23 +75,82 @@ var WorkerTypeTable = React.createClass({
             <th>Pending Capacity</th>
             <th>Requested Capacity</th>
             <th>Pending Tasks</th>
+            <th>Details/Edit</th>
           </tr>
         </thead>
         <tbody>
         {
           this.renderWaitFor('workerTypes') || this.state.workerTypes.map(function(name) {
-            return (<tr key={name}>
-              <td>{name}</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>);
+            return <WorkerTypeRow key={name} workerType={name} />;
           })
         }
         </tbody>
       </bs.Table>
     );
+  },
+});
+
+var WorkerTypeRow = React.createClass({
+  mixins: [
+    utils.createTaskClusterMixin({
+      clients: {
+        awsProvisioner: AwsProvisionerClient,
+        queue: taskcluster.Queue,
+      },
+    }),
+  ],
+
+  propTypes: {
+    workerType: React.PropTypes.string.isRequired,
+  },
+
+  getInitialState: function() {
+    return {
+      awsState: {
+        runningCapacity: 'loading',
+        pendingCapacity: 'loading',
+        requestedCapacity: 'loading',
+      },
+      awsStateLoaded: true,
+      awsStateError: undefined,
+      pendingTasks: {
+        pendingTasks: 'loading',  
+      },
+      pendingTasksLoaded: true,
+      pendingTasksError: undefined,
+    };
+  },
+
+  load: function() {
+    console.log('loading');
+    return {
+      awsState: Promise.resolve({
+        runningCapacity: 1,
+        pendingCapacity: 2,
+        requestedCapacity: 3
+      }),
+      pendingTasks: this.queue.pendingTasks(provisionerId, this.props.workerType),
+    };
+  },
+
+  render: function() {
+    return this.renderWaitFor('pendingTasks') ||
+           this.renderWaitFor('awsState') ||
+    (<tr>
+      <td>{this.props.workerType}</td>
+      <td>{this.state.awsState.runningCapacity}</td>
+      <td>{this.state.awsState.pendingCapacity}</td>
+      <td>{this.state.awsState.requestedCapacity}</td>
+      <td>{this.state.pendingTasks.pendingTasks}</td>
+      <td>
+        <bs.Button bsStyle='primary' bsSize='xsmall' onClick={this.handleButton}>
+        Edit</bs.Button>
+      </td>
+    </tr>);  
+  },
+
+  handleButton: function() {
+    alert('Pity da fool');
   },
 });
 
