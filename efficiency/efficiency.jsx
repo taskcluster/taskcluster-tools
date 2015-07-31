60d84eedcfb1eadc9775d0385397fd2c44d5d5dc
Edgar Chen <eggylv999@gmail.com>
2015-07-30 17:07:20 -0700

    added efficiency calculator with awful, -1/10 ui

---
+++ efficiency/efficiency.jsx
@@ -0,0 +1,91 @@
+let $ = require('jquery');
+var bs = require('react-bootstrap');
+let Promise = require('promise');
+let qs = require('querystring');
+var React = require('react');
+let request = require('superagent-promise');
+
+async function update(start, end) {
+  let url = 'https://goldiewilson-onepointtwentyone-1.c.influxdb.com:8087/db/docker-worker/series';
+  let res = await request.get(url).query({
+    u: 'efficiency',
+    p: 'uit8cx',
+    q: 'select * from capacity_over_time where time > ' + start + ' and time < ' + end
+  }).end();
+  console.log(res.status);
+  let queryResult = JSON.parse(res.text)[0];
+
+  let utilized = 0;
+  let total = 0;
+  let idleMachineTime = 0;
+  let totalMachineTime = 0;
+
+  let columns = queryResult.columns;
+  let runningIndex = columns.indexOf('runningTasks');
+  let idleIndex = columns.indexOf('idleCapacity');
+  let totalIndex = columns.indexOf('totalCapacity');
+  let durationIndex = columns.indexOf('duration');
+
+  for (let point of queryResult.points) {
+    utilized += point[runningIndex] * point[durationIndex];
+    total += point[totalIndex] * point[durationIndex];
+    totalMachineTime += point[durationIndex];
+    if (point[idleIndex] === point[totalIndex]) {
+      idleMachineTime += point[durationIndex];
+    }
+  }
+  document.getElementById('result').innerHTML =
+    (utilized / total * 100).toFixed(2) +
+    '% of total capacity is being used over the time period, ie efficiency<br>' +
+    ((1 - idleMachineTime / totalMachineTime) * 100).toFixed(2) + 
+    '% of workers are doing something over the time period';
+}
+
+var Efficiency = React.createClass({
+  render: function () {
+    return(
+      <span>
+      <h1>Efficiency calculator</h1>
+      <p>This tool lets approximates the efficiency of docker-worker over 
+      a certain period of time.</p>
+      <form className="form-horizontal" onSubmit={this.handleSubmit} id="form">
+        <bs.Input
+          type="text"
+          ref="start"
+          placeholder="start time"
+          defaultValue="now() - 1d"
+          labelClassName="col-sm-2"
+          wrapperClassName="col-sm-10"/>
+        <bs.Input
+          type="text"
+          ref="end"
+          placeholder="end time"
+          defaultValue="now()"
+          labelClassName="col-sm-2"
+          wrapperClassName="col-sm-10"/>
+        <div className="form-group">
+          <div className="col-sm-offset-2 col-sm-10">
+            <input type="submit"
+                   className="btn btn-primary"
+                   value="Query!"/>
+          </div>
+        </div>
+      </form>
+      <p id="result">
+      </p>
+      </span>
+    );
+  },
+  /** Handle form submission */
+  handleSubmit: function(e) {
+    e.preventDefault();
+    //TODO: figure out what to put in state
+    //this.setState({taskId: this.state.taskIdInput});
+    // console.log(document.getElementById("form").elements[0].value);
+    // console.log(document.getElementById("form").elements[1].value);
+    update(document.getElementById("form").elements[0].value, document.getElementById("form").elements[1].value);
+  }
+});
+
+module.exports = Efficiency;
+
