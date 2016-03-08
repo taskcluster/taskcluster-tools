9e82dcf3449e31b10deb26e328ef2a675967ed4d
Dustin J. Mitchell <dustin@mozilla.com>
2016-03-08 14:52:27 +0000

    Edit scopes in a textarea
    This allows copy/paste and other conveniences while editing scopes.

--- lib/ui/scopeeditor.jsx
+++ lib/ui/scopeeditor.jsx
@@ -25,67 +25,39 @@ var ScopeEditor = React.createClass({
     var scopes = _.clone(this.props.scopes || []);
     scopes.sort();
     return (
-      <div className="form-control-static">
-        <ul style={{paddingLeft: 20}}>
-          {
-            scopes.map((scope) => {
-              return (
-                <li key={scope}>
-                  <code>{scope}</code>
-                  &nbsp;
-                  <bs.Button
-                    className="role-editor-remove-scope-btn"
-                    bsStyle="danger"
-                    bsSize="xsmall"
-                    onClick={this.removeScope.bind(this, scope)}>
-                    <bs.Glyphicon glyph="trash"/>
-                  </bs.Button>
-                </li>
-              );
-            })
-          }
-        </ul>
-        <div className="input-group">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="new-scope:for-something:*"
-            ref="newScope"/>
-          <span className="input-group-btn">
-            <button className="btn btn-success"
-                    type="button" onClick={this.addScope}>
-              <bs.Glyphicon glyph="plus"/>
-              &nbsp;
-              Add
-            </button>
-          </span>
-        </div>
+      <div>
+        <textarea
+          className="form-control"
+          placeholder="new-scope:for-something:*"
+          rows={scopes.length + 1}
+          defaultValue={scopes.join("\n")}
+          onChange={this.onChange}
+          ref="scopeText"></textarea>
       </div>
     );
   },
 
-  /** Add scope to state */
-  addScope() {
-    var scope = this.refs.newScope.value;
-    // Let's skip empty strings
-    if (scope) {
-      var scopes = _.clone(this.props.scopes);
-      if (scopes.indexOf(scope) == -1) {
-        scopes.push(scope);
-        scopes.sort();
-        this.props.scopesUpdated(scopes);
-      }
-      this.refs.newScope.value = "";
+  getScopesFromTextarea() {
+    let newScopes = this.refs.scopeText.value.split(/[\r\n]+/);
+    newScopes = Array.from(new Set(newScopes.filter(s => s != "")));
+    newScopes.sort();
+    return newScopes;
+  },
+
+  componentDidUpdate(prevProps, prevState) {
+    // If we've gotten an update to the set of scopes, and it does
+    // not correspond to what's in the textarea right now, update
+    // the textarea, even at the cost of hurting user input
+    if (!_.isEqual(this.props.scopes, prevProps.scopes) &&
+        !_.isEqual(this.props.scopes, this.getScopesFromTextarea())) {
+      this.refs.scopeText.value = this.props.scopes.join("\n");
     }
   },
 
-  /** Remove a scope from state */
-  removeScope(scope) {
-    var scopes = _.clone(this.props.scopes);
-    var index = scopes.indexOf(scope);
-    if (index != -1) {
-      scopes.splice(index, 1);
-      this.props.scopesUpdated(scopes);
+  onChange() {
+    let newScopes = this.getScopesFromTextarea();
+    if (!_.isEqual(this.props.scopes, newScopes)) {
+      this.props.scopesUpdated(newScopes);
     }
   },
 
