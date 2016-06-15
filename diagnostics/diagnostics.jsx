4ed94dc06644c91b58be9bf01cd2f88894702862
Chinmay Kousik <chinmaykousik1@gmail.com>
2016-06-15 18:42:20 +0530

    Code cleanup and better looking ui

--- diagnostics/diagnostics.jsx
+++ diagnostics/diagnostics.jsx
@@ -1,96 +1,66 @@
-var React       = require('react');
-var retrieve    = require('./retrieve.js');
-var _           = require('lodash');
-var bs          = require('react-bootstrap');
+import React from 'react';
+import _ from 'lodash';
+import Service from './service.jsx';
+import Retrieve from './retrieve';
+import { Grid, Row } from 'react-bootstrap';
 
-const SERVICE_STYLE = {
-	backgroundColor: "#e3e3e3",
-	margin: "10px",
-	padding: "10px"
+export default class Diagnostics extends React.Component {
+
+  constructor (props) {
+    super(props);
+    
+    this.state = {
+      jsonLog:  '',
+      rawLog:   '',
+      testId:   '',
+      testDate: ''
+    }
+  }
+
+  async componentWillMount () {
+    let res = await Retrieve.getLatestLog('production', 'JSON');
+    let jsonLog = Retrieve.parseResult(JSON.parse(res.log));
+    let testId = res.testId;
+    let testDate = res.testDate;
+
+    this.setState({
+      jsonLog,
+      testId,
+      testDate
+    });
+
+    res = await Retrieve.getLatestLog('production', 'RAW');
+    let rawLog = res.log.replace(/\\n/g,'\n')
+      .replace(/\\"/g,'"')
+      .replace(/\\r/g,'\n')
+      .replace(/\\/g, '');
+
+    this.setState({ rawLog });
+  }
+
+  render () {
+    return <Grid> 
+      <h4>Date: {this.state.testDate}</h4>
+      <h4>TestId: {this.state.testId}</h4>
+      <Row>
+        {
+          _.keys(this.state.jsonLog).map(s => <Service key={s} title={s} test={this.state.jsonLog[s]} />)
+        }
+      </Row>
+      <h3>Log: </h3>
+      <RawLog text={this.state.rawLog} />
+    </Grid>
+  }
 }
 
-var Service = React.createClass ({
-  render: function () {
-    return <bs.Col style={SERVICE_STYLE} >
-      <p><b>{this.props.title}</b></p>
-      {(this.props.test.pass.length)? this.showPassing() : ''}
-      {(this.props.test.fail.length)? this.showFailing() : ''}
-      </bs.Col>
-  },
-
-  showPassing: function(){
-    return <div> 
-      <p>Passing: </p>
-      <ul className="container-fluid pass">
-       {
-          this.props.test.pass.map(res => {
-            return <li>{res}</li>
-          })
-       }
-      </ul>
-    </div>
-  },
-
-  showFailing: function(){
-    return <div> 
-      <p>Failing: </p>
-      <ul>
-        {
-          this.props.test.fail.map(res => <li>{res}</li>)
-        }
-      </ul>
-    </div>
-  }
-});
-
-var Diagnostics = React.createClass({
-  getInitialState: () => {
-    return {
-      testResult: {},
-      testId:'',
-      error: null,
-      rawLog: ''
-    };
-  },
-
-  componentWillMount: function() {
-    var that = this;
-    retrieve.getLatestLog('production','JSON').then(res => {
-      let jsonres = retrieve.parseResult(JSON.parse(res.log));
-      that.setState({
-        testDate: res.testDate,
-        testId: res.testId,
-        testResult: jsonres
-      });
-    });
-    retrieve.getLatestLog('production','RAW').then(res => {
-      var rawLog = (res.log).replace(/\\n/g,"\n")
-        .replace(/\\"/g,'"')
-        .replace(/\\r/g,"\n")
-        .replace(/\\/,"");
-      that.setState({
-        rawLog
-      });
-    })
-  },
-
-  render:function () {
-    return <bs.Grid>
-      <h3>TestId: {this.state.testId}</h3>
-      <h3>Date: {this.state.testDate}</h3> 
-      <bs.Row>
-        {
-          _.keys(this.state.testResult).map(service => {
-            return <Service key={service} title={service} test={this.state.testResult[service]}/>
-          })
-        }
-      </bs.Row>
-      <h3>Logs: </h3>
-      <p>{this.state.rawLog}</p>
-    </bs.Grid>  
-  }
-
-});
-
-module.exports = Diagnostics;
-
+const RawLog = props => {
+  return <div className="log">
+    {
+      props.text.split('\n').map(l =>{
+        if(l.indexOf("\u2713") != -1)
+          return <p className="success">{l}</p>
+        return <p className="message">{l}</p>
+      })
+    }
+  </div>
+} 
