a7777d50a1036039c415478f0bdc1ea7bf96838e
helfi92 <helfi92@gmail.com>
2016-07-01 13:00:48 -0700

    add buffer implementation for incoming listener messages

--- push-inspector/containers/listings.jsx
+++ push-inspector/containers/listings.jsx
@@ -12,8 +12,12 @@ class Listings extends Component {
   constructor(props) {
     super(props);
     this.listener = null;
+    
+    this.bTimer = null;
+    this.bQueue = [];
 
     this.handleMessage = this.handleMessage.bind(this);
+
   }
 
   /** Close Listener connection */
@@ -52,27 +56,100 @@ class Listings extends Component {
  
   }
 
+  getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor( (t/1000) % 60 );
+    var minutes = Math.floor( (t/1000/60) % 60 );
+    var hours = Math.floor( (t/(1000*60*60)) % 24 );
+    var days = Math.floor( t/(1000*60*60*24) );
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+
   /** Handle message from the queue */
   handleQueueMessage(message) {
     const { params, fetchArtifacts, fetchTasks, fetchTask, fetchStatus } = this.props;
     const { taskId, taskGroupId } = params;
-    
-    fetchTasks(taskGroupId);
+    const durationToWait = 5;
+    let toExtract = 10;
 
-    if(taskId) {
-      if(message.exchange == queueEvents.artifactCreated().exchange) {
-      fetchArtifacts(taskId);
-      return;
+    // babel makes 'this' to be undefined inside arrow functions, hence the use of 'that'
+    let that = this;
+
+    //  Case where messages stop coming and won't be able to make the required calls
+    const edgeCase = () => {
+      let flag = false;
+      return function() {
+        if(flag = false) {
+          flag = true;
+          console.log('setting a timer...');
+          setTimeout(() => {            
+            if(that.bQueue.length > 0) {
+              update();
+              flag = false;
+            }
+          }, durationToWait * 1000);
+        }
+      }
+    }
+
+    const update = () => {
+      // up to toExtract calls from the buffer
+      while(true) {
+        toExtract -= 1;
+        let elem = that.bQueue.shift();
+
+        // Update Task
+        if(taskId) {   
+          updateTask(elem);
+        }
+
+        // Update tasks list if and only if on the last turn of the loop
+        if(!(that.bQueue.length > 0 && toExtract > 0)) {
+          console.log('updating tasks lists');
+          updateTasksList();
+          return;  
+        }      
+      }
+    }
+
+    const updateTask = (elem) => {
+      if(elem.exchange == queueEvents.artifactCreated().exchange) {
+        fetchArtifacts(taskId);
+        return;
       }
 
-    
       fetchTask(taskId);
       fetchStatus(taskId);
     }
 
+    const updateTasksList = () => {
+      fetchTasks(taskGroupId);
+    }
+
+
+    // Append message to buffer queue
+    this.bQueue.push(message);
+       
+    // Create a date with current time
+    if(this.bTimer == null) {
+      this.bTimer = new Date();
+
+    // Check if 'durationToWait' seconds has elapsed  
+    } else if(new Date(new Date() - this.bTimer).getSeconds() >= durationToWait) {
+      console.log('5 seconds have elapsed');
+      // Stop timer
+      this.bTimer = null;
+      update();    
+    }
+    edgeCase();              
   }
-
-
 
   /** Remove the list of tasks that were previously loaded */
   componentWillUnmount() {
