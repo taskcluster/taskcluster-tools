34a364229775349cbd8a51b30ce5cf9c7fe1a2bd
Jonas Finnemann Jensen <jopsen@gmail.com>
2016-09-20 14:44:41 -0700

    Strip self-dependencies when retriggering

--- lib/ui/retrigger-button.jsx
+++ lib/ui/retrigger-button.jsx
@@ -48,7 +48,8 @@ let RetriggerButton = React.createClass({
         <code>taskId</code>.<br/><br/>
         The new task will be altered as to:
         <ul>
-          <li>Update deadlines and other timestamps for the current time.</li>
+          <li>Update deadlines and other timestamps for the current time, and,</li>
+          <li>Strip self-dependencies from the task definition.</li>
         </ul>
         Note: this may not work with all tasks.
       </ConfirmAction>
@@ -64,6 +65,7 @@ let RetriggerButton = React.createClass({
     task.deadline = new Date(now + Date.parse(task.deadline) - created).toJSON();
     task.expires = new Date(now + Date.parse(task.expires) - created).toJSON();
     task.created = new Date(now).toJSON();
+    task.dependencies = task.dependencies.filter(requiredTask => requiredTask != taskId);
 
     task.retries = 0;
 
