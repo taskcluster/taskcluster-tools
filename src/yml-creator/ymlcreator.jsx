505b0cfee7e5f226488fa739ac3d236624034d6d
owlishDeveloper <adamanna.ca@gmail.com>
2017-01-26 16:33:57 -0800

    Finished interface. Draft

--- src/yml-creator/ymlcreator.jsx
+++ src/yml-creator/ymlcreator.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
+import {
+  Grid,
+  Row,
+  Col,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  Checkbox,
+  ButtonToolbar,
+  Button,
+  Glyphicon,
+} from 'react-bootstrap';
 
 export default class YmlCreator extends React.Component {
   render() {
@@ -13,7 +24,7 @@ export default class YmlCreator extends React.Component {
               which should live at the root of your repository. It defines
               tasks that you want TaskCluster to run for you. The tasks will run when certain 
               GitHub events happen — you will choose the events you're interested in while 
-              creating the file. After pressing <b>Create</b> button, you will see the contents of 
+              creating the file. After pressing <b>Create file</b> button, you will see the contents of 
               your file appear at the bottom of this page. All you need to do is to copy it, go to 
               your repository, create a file at its root, paste and save 
               as <code>.taskcluster.yml</code>. Optionally, after you create your file, you can edit 
@@ -29,13 +40,90 @@ export default class YmlCreator extends React.Component {
             <hr />
           </Col>
         </Row>
-          <Col sm={12}>
+
+        <Row>
+          <h5>Enter the name and desciption of your project or these tasks:</h5>
+          <FormGroup>
+            <ControlLabel>Name:</ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="Name"
+              value=""
+              ref="rootName"
+              onChange="" />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Description:</ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="Description"
+              value=""
+              ref="rootDescription"
+              onChange="" />
+          </FormGroup>
+        </Row>
+
+        <hr />
+
+        <Row>
+          <h5>Define your task:</h5>
+          <Col md={5}>
             <FormGroup>
-              <h5>Enter the name and desciption of your project or these tasks:</h5>
+              <ControlLabel>Name:</ControlLabel>
+              <FormControl
+                type="text"
+                placeholder="Name of the task"
+                value=""
+                ref="taskName"
+                onChange="" />
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel>Description:</ControlLabel>
+              <FormControl
+                type="text"
+                placeholder="Description of the task"
+                value=""
+                ref="taskDescription"
+                onChange="" />
             </FormGroup>
           </Col>
-        <Row>
+          <Col md={7}>
+            <FormGroup>
+              <ControlLabel>Events:</ControlLabel>
+              <Checkbox>Pull request opened</Checkbox>
+              <Checkbox>Pull request merged or closed</Checkbox>
+              <Checkbox>Push</Checkbox>
+              <Checkbox>Release or tag created</Checkbox>
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel>Language:</ControlLabel>
+              <FormControl componentClass="select" placeholder="Pick one...">
+                <option value="node">JavaScript with Node.js</option>
+                <option value="python">Python</option>
+              </FormControl>
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel>Commands: </ControlLabel>
+              <FormControl componentClass="select" placeholder="Pick one...">
+                <option value="standard">Clone repo and run my tests</option>
+                <option value="custom">I need something fussier</option>
+              </FormControl>
+            </FormGroup>
+          </Col>
         </Row>
+
+        <hr />
+
+        <ButtonToolbar>
+          <Button
+            bsStyle="success">
+            <Glyphicon glyph="plus" /> Define another task
+          </Button>
+          <Button
+            bsStyle="primary">
+            <Glyphicon glyph="ok" /> Create file
+          </Button>
+        </ButtonToolbar>
       </Grid>
     );
   }
