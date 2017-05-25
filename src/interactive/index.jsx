247872691c25d95cbba3db96faad08c0c0a4d332
helfi92 <helfi92@gmail.com>
2017-05-25 09:11:10 -0400

    Turn src/interactive and src/shell into React classes
    Since we are using a single page application, having
    `src/interactive/index.js` and `src/shell/index.js`
    not in a React class will cause both files to be loaded simultaneously.
    In order to avoid that, both files needs to be transformed
    into a React class so that React Router only loads the
    respective component.
    
    Also, a manual download of utf-8-validate and bufferutil
    had to be done to make both terminals work.

--- src/interactive/index.jsx
+++ src/interactive/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Layout from '../lib/Layout';
 import {hterm, lib} from 'hterm-umd';
 import url from 'url';
@@ -6,80 +6,88 @@ import qs from 'querystring';
 import {DockerExecClient} from 'docker-exec-websocket-server';
 import './interactive.less';
 
-const args = qs.parse(url.parse(window.location.href).query);
+class Interactive extends Component {
+  constructor(props) {
+    super(props);
 
-// Set default storage engine for hterm
-hterm.defaultStorage = new lib.Storage.Local();
+    const args = qs.parse(url.parse(window.location.href).query);
 
-const Terminal = () => (
-  <Layout>
-    <div id="terminal" />
-  </Layout>
-);
+    // Set default storage engine for hterm
+    hterm.defaultStorage = new lib.Storage.Local();
 
-export default Terminal;
+    document.addEventListener("DOMContentLoaded", () => {
+      const term = new hterm.Terminal('interactive');
 
-const term = new hterm.Terminal('interactive');
+      term.onTerminalReady = async () => {
+        const io = term.io.push();
+        const client = new DockerExecClient({
+          url: args.socketUrl,
+          tty: true,
+          command: [
+            'sh', '-c', [
+              'if [ -z "$TERM" ]; then export TERM=xterm; fi;',
+              'if [ -z "$HOME" ]; then export HOME=/root; fi;',
+              'if [ -z "$USER" ]; then export USER=root; fi;',
+              'if [ -z "$LOGNAME" ]; then export LOGNAME=root; fi;',
+              'if [ -z `which "$SHELL"` ]; then export SHELL=bash; fi;',
+              'if [ -z `which "$SHELL"` ]; then export SHELL=sh; fi;',
+              'if [ -z `which "$SHELL"` ]; then export SHELL="/.taskclusterutils/busybox sh"; fi;',
+              'SPAWN="$SHELL";',
+              'if [ "$SHELL" = "bash" ]; then SPAWN="bash -li"; fi;',
+              'exec $SPAWN;',
+            ].join(''),
+          ],
+        });
 
-term.onTerminalReady = async () => {
-  const io = term.io.push();
-  const client = new DockerExecClient({
-    url: args.socketUrl,
-    tty: true,
-    command: [
-      'sh', '-c', [
-        'if [ -z "$TERM" ]; then export TERM=xterm; fi;',
-        'if [ -z "$HOME" ]; then export HOME=/root; fi;',
-        'if [ -z "$USER" ]; then export USER=root; fi;',
-        'if [ -z "$LOGNAME" ]; then export LOGNAME=root; fi;',
-        'if [ -z `which "$SHELL"` ]; then export SHELL=bash; fi;',
-        'if [ -z `which "$SHELL"` ]; then export SHELL=sh; fi;',
-        'if [ -z `which "$SHELL"` ]; then export SHELL="/.taskclusterutils/busybox sh"; fi;',
-        'SPAWN="$SHELL";',
-        'if [ "$SHELL" = "bash" ]; then SPAWN="bash -li"; fi;',
-        'exec $SPAWN;',
-      ].join(''),
-    ],
-  });
+        io.onVTKeystroke = io.sendString = d => {
+          client.stdin.write(d);
+        };
+        io.onTerminalResize = () => null;
 
-  io.onVTKeystroke = io.sendString = d => {
-    client.stdin.write(d);
-  };
-  io.onTerminalResize = () => null;
+        term.setCursorPosition(0, 0);
+        term.setCursorVisible(true);
+        term.setScrollbarVisible(false);
+        /* eslint-disable no-underscore-dangle */
+        term.prefs_.set('ctrl-c-copy', true);
+        term.prefs_.set('ctrl-v-paste', true);
+        term.prefs_.set('use-default-window-copy', true);
+        /* eslint-enable no-underscore-dangle */
 
-  term.setCursorPosition(0, 0);
-  term.setCursorVisible(true);
-  term.setScrollbarVisible(false);
-  /* eslint-disable no-underscore-dangle */
-  term.prefs_.set('ctrl-c-copy', true);
-  term.prefs_.set('ctrl-v-paste', true);
-  term.prefs_.set('use-default-window-copy', true);
-  /* eslint-enable no-underscore-dangle */
+        await client.execute();
 
-  await client.execute();
+        term.installKeyboard();
+        io.writeUTF8(`Connected to remote shell for taskId: ${args.taskId}\r\n`);
 
-  term.installKeyboard();
-  io.writeUTF8(`Connected to remote shell for taskId: ${args.taskId}\r\n`);
+        client.on('exit', code => {
+          io.writeUTF8(`\r\nRemote shell exited: ${code}\r\n`);
+          term.uninstallKeyboard();
+          term.setCursorVisible(false);
+        });
 
-  client.on('exit', code => {
-    io.writeUTF8(`\r\nRemote shell exited: ${code}\r\n`);
-    term.uninstallKeyboard();
-    term.setCursorVisible(false);
-  });
+        client.resize(term.screenSize.height, term.screenSize.width);
 
-  client.resize(term.screenSize.height, term.screenSize.width);
+        io.onTerminalResize = (c, r) => {
+          client.resize(r, c);
+        };
+        client.stdout.on('data', data => {
+          io.writeUTF8(data.toString('utf8'));
+        });
+        client.stderr.on('data', data => {
+          io.writeUTF8(data.toString('utf8'));
+        });
+      };
 
-  io.onTerminalResize = (c, r) => {
-    client.resize(r, c);
-  };
-  client.stdout.on('data', data => {
-    io.writeUTF8(data.toString('utf8'));
-  });
-  client.stderr.on('data', data => {
-    io.writeUTF8(data.toString('utf8'));
-  });
-};
+      term.decorate(document.getElementById('terminal'));
+    });
+  }
 
-const decorate = () => term.decorate(document.getElementById('terminal'));
+  render() {
+    return (
+      <Layout>
+        <div id="terminal" />
+      </Layout>
+    );
+  }
+}
 
-setTimeout(decorate, 1000);
+export default Interactive;
