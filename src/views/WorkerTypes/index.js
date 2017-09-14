9a9d5d451cd7b5d8d912f34918010fbe25415a9e
Hassan Ali <helfi92@gmail.com>
2017-09-14 14:04:27 -0400

    Bug 1399566 - Worker Types Keep settings when refreshed

--- src/views/WorkerTypes/index.js
+++ src/views/WorkerTypes/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Clients from '../../components/Clients';
 import WorkerManager from './WorkerManager';
 
-const View = ({ credentials, history, match }) => (
+const View = ({ credentials, history, match, location }) => (
   <Clients credentials={credentials} Queue AwsProvisioner={{ baseUrl: 'https://aws-provisioner.taskcluster.net/v1' }}>
     {clients => (
       <WorkerManager
         {...clients}
         history={history}
-        provisionerId={match.params.provisionerId ? decodeURIComponent(match.params.provisionerId) : ''} />
+        location={location}
+        provisionerId={match.params.provisionerId ? match.params.provisionerId : ''} />
     )}
   </Clients>
 );
