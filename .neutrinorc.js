a20f6a9c880f594a45ef18f4076376ea50089c7c
Dustin J. Mitchell <dustin@mozilla.com>
2017-09-19 22:00:18 +0000

    Use sourcemap for development builds

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -129,7 +129,7 @@ module.exports = {
   ],
   env: {
     NODE_ENV: {
-      development: ({ config }) => config.devtool('eval')
+      development: ({ config }) => config.devtool('sourcemap')
     }
   }
 };
