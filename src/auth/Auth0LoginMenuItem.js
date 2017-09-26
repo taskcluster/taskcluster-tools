a7e6dbc5e32adec80c66079f88773f22a569ca03
Dustin J. Mitchell <dustin@mozilla.com>
2017-09-26 14:35:46 +0000

    add tooltip

--- src/auth/Auth0LoginMenuItem.js
+++ src/auth/Auth0LoginMenuItem.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { NavItem, Glyphicon } from 'react-bootstrap';
+import { OverlayTrigger, Tooltip, NavItem, Glyphicon } from 'react-bootstrap';
 
 // This authenticates to Auth0 by opening a new Window where Auth0 will do its
 // thing, then closing that window when login is complete.
 export default class Auth0LoginMenuItem extends React.PureComponent {
   render() {
+    const tooltip = (
+      <Tooltip id="auth0-signin">
+        We are testing this login method, which will be live in October.  Please try it and report any issues!
+      </Tooltip>
+    );
+
     return (
-      <NavItem onSelect={this.onSelect}>
-        <Glyphicon glyph="log-in" /> Sign In with Auth0
-      </NavItem>
+      <OverlayTrigger placement="left" delay={600} overlay={tooltip}>
+        <NavItem onSelect={this.onSelect}>
+          <Glyphicon glyph="log-in" /> Sign In with Auth0
+        </NavItem>
+      </OverlayTrigger>
     );
   }
 
