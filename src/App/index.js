32d0b6c267b6ad1ab6950838a45de826b508245d
Dustin J. Mitchell <dustin@mozilla.com>
2017-09-28 19:21:39 +0000

    Bug 1403704 - when auth renewal is overdue, put up a spinner
    This avoids trying to make API calls before we've refreshed the
    accessToken (which results in a call to login.tc.n that fails)

--- src/App/index.js
+++ src/App/index.js
@@ -9,6 +9,7 @@ import { loadable } from '../utils';
 import './styles.css';
 import iconUrl from '../taskcluster.png';
 import LegacyRedirect from './LegacyRedirect';
+import Spinner from '../components/Spinner';
 import AuthController from '../auth/AuthController';
 
 const Home = loadable(() =>
@@ -98,24 +99,37 @@ export default class App extends React.Component {
     super(props);
     this.authController = new AuthController();
     this.state = {
-      userSession: null
+      userSession: null,
+      authReady: false
     };
   }
 
   componentWillMount() {
-    this.authController.onUserSessionChanged(userSession =>
-      this.setState({ userSession })
-    );
+    this.authController.onUserSessionChanged(this.handleUserSessionChanged);
 
     // we do not want to automatically load a user session on the login views; this is
     // a hack until they get an entry point of their own with no UI.
     if (!window.location.pathname.startsWith('/login')) {
       this.authController.loadUserSession();
+    } else {
+      this.setState({ authReady: true });
     }
   }
 
+  handleUserSessionChanged = userSession => {
+    // Consider auth "ready" when we have no userSession, a userSession with no
+    // renewAfter, or a renewAfter that is not in the past.  Once auth is
+    // ready, it never becomes non-ready again.
+    const authReady =
+      this.state.authReady ||
+      !userSession ||
+      !userSession.renewAfter ||
+      new Date(userSession.renewAfter) > new Date();
+    this.setState({ userSession, authReady });
+  };
+
   render() {
-    const { userSession } = this.state;
+    const { authReady, userSession } = this.state;
     const { authController } = this;
 
     return (
@@ -130,186 +144,197 @@ export default class App extends React.Component {
             authController={authController}
           />
           <Grid fluid id="container">
-            <Switch>
-              <PropsRoute path="/task-inspector" component={LegacyRedirect} />
-              <PropsRoute
-                path="/task-graph-inspector"
-                component={LegacyRedirect}
-              />
-              <PropsRoute
-                path="/task-group-inspector"
-                component={LegacyRedirect}
-              />
-              <PropsRoute path="/push-inspector" component={LegacyRedirect} />
-              <PropsRoute path="/one-click-loaner" component={LegacyRedirect} />
-              <PropsRoute path="/interactive" component={LegacyRedirect} />
-              <PropsRoute path="/task-creator" component={LegacyRedirect} />
+            {authReady ? (
+              <Switch>
+                <PropsRoute path="/task-inspector" component={LegacyRedirect} />
+                <PropsRoute
+                  path="/task-graph-inspector"
+                  component={LegacyRedirect}
+                />
+                <PropsRoute
+                  path="/task-group-inspector"
+                  component={LegacyRedirect}
+                />
+                <PropsRoute path="/push-inspector" component={LegacyRedirect} />
+                <PropsRoute
+                  path="/one-click-loaner"
+                  component={LegacyRedirect}
+                />
+                <PropsRoute path="/interactive" component={LegacyRedirect} />
+                <PropsRoute path="/task-creator" component={LegacyRedirect} />
 
-              <PropsRoute
-                path="/"
-                exact={true}
-                component={Home}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/tasks/create/interactive"
-                component={TaskCreator}
-                userSession={userSession}
-                interactive={true}
-              />
-              <PropsRoute
-                path="/tasks/create"
-                component={TaskCreator}
-                userSession={userSession}
-                interactive={false}
-              />
-              <PropsRoute
-                path="/tasks/:taskId/connect"
-                component={InteractiveConnect}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/tasks/:taskId?/:action?"
-                component={TaskRedirect}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/groups/:taskGroupId?/:groupSection?/:taskId?/:sectionId?/:runId?/:subSectionId?/:artifactId?"
-                component={UnifiedInspector}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/quickstart"
-                component={QuickStart}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/aws-provisioner/:workerType?/:currentTab?"
-                component={AwsProvisioner}
-                userSession={userSession}
-                baseUrl="https://aws-provisioner.taskcluster.net/v1"
-                provisionerId="aws-provisioner-v1"
-                routeRoot="/aws-provisioner"
-              />
-              <PropsRoute
-                path="/aws-provisioner-staging/:workerType?/:currentTab?"
-                component={AwsProvisioner}
-                userSession={userSession}
-                baseUrl="https://provisioner-staging.herokuapp.com/v1"
-                provisionerId="staging-aws"
-                routeRoot="/aws-provisioner-staging"
-              />
-              <PropsRoute
-                path="/workers/provisioners/:provisionerId?/worker-types/:workerType/workers/:workerGroup?/:workerId?"
-                component={WorkerManager}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/workers/:provisionerId?/:provisionerId/worker-types/:workerType"
-                component={Workers}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/workers/provisioners/:provisionerId?"
-                component={WorkerTypes}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/auth/clients/:clientId?"
-                component={ClientManager}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/auth/roles/:roleId?"
-                component={RoleManager}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/auth/scopes/:selectedScope?/:selectedEntity?"
-                component={ScopeInspector}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/pulse-inspector"
-                component={PulseInspector}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/purge-caches"
-                component={CachePurgeInspector}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/index/artifacts/:namespace?/:namespaceTaskId?"
-                component={IndexedArtifactBrowser}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/index/:namespace?/:namespaceTaskId?"
-                component={IndexBrowser}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/hooks/:hookGroupId?/:hookId?"
-                component={HooksManager}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/secrets/:secretId?"
-                component={SecretsManager}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/diagnostics"
-                component={Diagnostics}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/credentials"
-                component={CredentialsManager}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/display"
-                component={Displays}
-                userSession={userSession}
-              />
-              <PropsRoute
-                path="/shell"
-                component={Shell}
-                userSession={userSession}
-              />
-              {authController.canSignInUsing('auth0') && (
                 <PropsRoute
-                  path="/login/auth0"
-                  component={Auth0Login}
-                  setUserSession={authController.setUserSession}
+                  path="/"
+                  exact={true}
+                  component={Home}
+                  userSession={userSession}
                 />
-              )}
-              {authController.canSignInUsing('development') && (
                 <PropsRoute
-                  path="/login/development"
-                  component={DevelopmentLogin}
-                  setUserSession={authController.setUserSession}
+                  path="/tasks/create/interactive"
+                  component={TaskCreator}
+                  userSession={userSession}
+                  interactive={true}
                 />
-              )}
-              {authController.canSignInUsing('manual') && (
                 <PropsRoute
-                  path="/login/manual"
-                  component={ManualLogin}
-                  setUserSession={authController.setUserSession}
+                  path="/tasks/create"
+                  component={TaskCreator}
+                  userSession={userSession}
+                  interactive={false}
                 />
-              )}
-              {(authController.canSignInUsing('okta') ||
-                authController.canSignInUsing('email')) && (
                 <PropsRoute
-                  path="/login"
-                  component={Login}
-                  setUserSession={authController.setUserSession}
+                  path="/tasks/:taskId/connect"
+                  component={InteractiveConnect}
+                  userSession={userSession}
                 />
-              )}
-              <Route component={NotFound} />
-            </Switch>
+                <PropsRoute
+                  path="/tasks/:taskId?/:action?"
+                  component={TaskRedirect}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/groups/:taskGroupId?/:groupSection?/:taskId?/:sectionId?/:runId?/:subSectionId?/:artifactId?"
+                  component={UnifiedInspector}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/quickstart"
+                  component={QuickStart}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/aws-provisioner/:workerType?/:currentTab?"
+                  component={AwsProvisioner}
+                  userSession={userSession}
+                  baseUrl="https://aws-provisioner.taskcluster.net/v1"
+                  provisionerId="aws-provisioner-v1"
+                  routeRoot="/aws-provisioner"
+                />
+                <PropsRoute
+                  path="/aws-provisioner-staging/:workerType?/:currentTab?"
+                  component={AwsProvisioner}
+                  userSession={userSession}
+                  baseUrl="https://provisioner-staging.herokuapp.com/v1"
+                  provisionerId="staging-aws"
+                  routeRoot="/aws-provisioner-staging"
+                />
+                <PropsRoute
+                  path="/workers/provisioners/:provisionerId?/worker-types/:workerType/workers/:workerGroup?/:workerId?"
+                  component={WorkerManager}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/workers/:provisionerId?/:provisionerId/worker-types/:workerType"
+                  component={Workers}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/workers/provisioners/:provisionerId?"
+                  component={WorkerTypes}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/auth/clients/:clientId?"
+                  component={ClientManager}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/auth/roles/:roleId?"
+                  component={RoleManager}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/auth/scopes/:selectedScope?/:selectedEntity?"
+                  component={ScopeInspector}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/pulse-inspector"
+                  component={PulseInspector}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/purge-caches"
+                  component={CachePurgeInspector}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/index/artifacts/:namespace?/:namespaceTaskId?"
+                  component={IndexedArtifactBrowser}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/index/:namespace?/:namespaceTaskId?"
+                  component={IndexBrowser}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/hooks/:hookGroupId?/:hookId?"
+                  component={HooksManager}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/secrets/:secretId?"
+                  component={SecretsManager}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/diagnostics"
+                  component={Diagnostics}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/credentials"
+                  component={CredentialsManager}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/display"
+                  component={Displays}
+                  userSession={userSession}
+                />
+                <PropsRoute
+                  path="/shell"
+                  component={Shell}
+                  userSession={userSession}
+                />
+                {authController.canSignInUsing('auth0') && (
+                  <PropsRoute
+                    path="/login/auth0"
+                    component={Auth0Login}
+                    setUserSession={authController.setUserSession}
+                  />
+                )}
+                {authController.canSignInUsing('development') && (
+                  <PropsRoute
+                    path="/login/development"
+                    component={DevelopmentLogin}
+                    setUserSession={authController.setUserSession}
+                  />
+                )}
+                {authController.canSignInUsing('manual') && (
+                  <PropsRoute
+                    path="/login/manual"
+                    component={ManualLogin}
+                    setUserSession={authController.setUserSession}
+                  />
+                )}
+                {(authController.canSignInUsing('okta') ||
+                  authController.canSignInUsing('email')) && (
+                  <PropsRoute
+                    path="/login"
+                    component={Login}
+                    setUserSession={authController.setUserSession}
+                  />
+                )}
+                <Route component={NotFound} />
+              </Switch>
+            ) : (
+              <div style={{ textAlign: 'center' }}>
+                <Spinner />
+                <br />
+                Authenticating...
+              </div>
+            )}
           </Grid>
         </div>
       </BrowserRouter>
