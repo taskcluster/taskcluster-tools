dd52049e67fc6271c4f176650bdcb14bdf0eb9a9
Dustin J. Mitchell <dustin@mozilla.com>
2017-09-29 17:22:10 +0000

    Bug 1400550 - leave the Error component better than I found it
    Better handling for InsufficientScopes errors, and avoid suggesting
    signing in when the user might already be signed in.

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Alert, Button, Collapse } from 'react-bootstrap';
 import Markdown from '../Markdown';
 
@@ -26,14 +27,30 @@ export default class Error extends React.PureComponent {
 
   render() {
     const { error } = this.props;
+    const { open } = this.state;
 
-    if (error.response && error.response.status === 403) {
+    // Handle the common 403 error more neatly, especially since its msessage is large but
+    // sometimes useful.
+    if (
+      error.response &&
+      error.response.status === 403 &&
+      error.body.code === 'InsufficientScopes'
+    ) {
       return (
         <Alert bsStyle="danger">
           <p>
             You are not authorized to perform the requested action. Please sign
-            in and try again.
+            in and try again, or verify your scopes in the{' '}
+            <Link to="/credentials">Credentials Manager</Link>.
           </p>
+          <Button bsSize="xsmall" onClick={this.handleOpen}>
+            Additional details...
+          </Button>
+          <Collapse in={open}>
+            <div>
+              <Markdown>{error.message}</Markdown>
+            </div>
+          </Collapse>
         </Alert>
       );
     }
@@ -42,14 +59,18 @@ export default class Error extends React.PureComponent {
       <Alert bsStyle="danger">
         <strong>{this.getTitle()}</strong>
         <Markdown>{error.message}</Markdown>
-        {process.env.NODE_ENV === 'development' && <pre>{error.stack}</pre>}
+        {process.env.NODE_ENV === 'development' && (
+          <div>
+            Stack (development only):<pre>{error.stack}</pre>
+          </div>
+        )}
         {error.body &&
           error.body.requestInfo && (
             <div>
               <Button bsSize="xsmall" onClick={this.handleOpen}>
                 Additional details...
               </Button>
-              <Collapse in={this.state.open}>
+              <Collapse in={open}>
                 <pre>{JSON.stringify(error.body.requestInfo, null, 2)}</pre>
               </Collapse>
             </div>
