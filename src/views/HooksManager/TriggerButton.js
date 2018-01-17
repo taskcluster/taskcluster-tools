de87e36c088c1a2691eb19d678a9b22927ecf479
Dustin J. Mitchell <dustin@mozilla.com>
2018-01-17 03:03:41 +0000

    Bug 143089 - disable the submit button when not valid

--- src/views/HooksManager/TriggerButton.js
+++ src/views/HooksManager/TriggerButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { string, object, func } from 'prop-types';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Alert } from 'react-bootstrap';
 import { safeLoad, safeDump } from 'js-yaml';
 import Code from '../../components/Code';
 import CodeEditor from '../../components/CodeEditor';
@@ -24,33 +24,40 @@ export default class TriggerButton extends React.PureComponent {
 
   handleSubmit = () => {
     if (!this.state.contextValid) {
-      // TODO: disbale "Trigger Hook" button in the modal when not valid
-      throw new Error('Trigger context is not valid YAML');
+      throw new Error('Trigger context is not valid');
     } else {
       this.props.onTrigger(this.state.context);
     }
   };
 
   handleContextChange = value => {
+    let context;
+
     try {
-      this.setState({
-        context: safeLoad(value),
-        contextValid: true
-      });
+      context = safeLoad(value);
     } catch (e) {
-      this.setState({
+      return this.setState({
         context: null,
-        contextValid: false
+        contextValid: false,
+        validityMessage: 'Trigger context is not valid YAML'
       });
     }
+
+    this.setState({
+      context,
+      contextValid: true,
+      validityMessage: undefined
+    });
   };
 
   render() {
+    const { hookGroupId, hookId, schema } = this.props;
+    const { validityMessage } = this.state;
     const triggerModal = (
       <div>
         Trigger Hook{' '}
         <tt>
-          {this.props.hookGroupId}/{this.props.hookId}
+          {hookGroupId}/{hookId}
         </tt>
         with the following trigger context:
         <Row>
@@ -62,21 +69,29 @@ export default class TriggerButton extends React.PureComponent {
               value={'{}'}
               onChange={this.handleContextChange}
             />
+            {validityMessage && (
+              <Alert bsStyle="warning">{validityMessage}</Alert>
+            )}
           </Col>
           <Col lg={6} md={6} sm={12}>
             <h4>Trigger Schema</h4>
             <Code
               language="yaml"
               style={{ maxHeight: 250, overflow: 'scroll' }}>
-              {safeDump(this.props.schema)}
+              {safeDump(schema)}
             </Code>
           </Col>
+          <pre>{JSON.stringify(this.state, null, 2)}</pre>
         </Row>
       </div>
     );
 
     return (
-      <ModalItem onSubmit={this.handleSubmit} button={true} body={triggerModal}>
+      <ModalItem
+        submitDisabled={!this.state.contextValid}
+        onSubmit={this.handleSubmit}
+        button={true}
+        body={triggerModal}>
         {this.props.children}
       </ModalItem>
     );
