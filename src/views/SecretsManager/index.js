19d3692346856613abb3877f47ecdac4bc4c8ebe
JQuinnie <jenn8866@gmail.com>
2018-02-21 14:09:13 -0600

    Add const to pass down props

--- src/views/SecretsManager/index.js
+++ src/views/SecretsManager/index.js
@@ -3,25 +3,30 @@ import WithClients from '../../components/WithClients';
 import WithUserSession from '../../components/WithUserSession';
 import SecretsManager from './SecretsManager';
 
-const View = ({ history, match }) => (
-  <WithUserSession>
-    {userSession => (
-      <WithClients Secrets>
-        {clients => (
-          <SecretsManager
-            history={history}
-            {...clients}
-            userSession={userSession}
-            secretId={
-              match.params.secretId
-                ? decodeURIComponent(match.params.secretId)
-                : ''
-            }
-          />
-        )}
-      </WithClients>
-    )}
-  </WithUserSession>
-);
+const View = ({ history, match }) => {
+  const selectedSecret = decodeURIComponent(match.params.selectedSecret || '');
+
+  return (
+    <WithUserSession>
+      {userSession => (
+        <WithClients Secrets>
+          {clients => (
+            <SecretsManager
+              history={history}
+              {...clients}
+              userSession={userSession}
+              secretId={
+                match.params.secretId
+                  ? decodeURIComponent(match.params.secretId)
+                  : ''
+              }
+              selectedSecret={selectedSecret}
+            />
+          )}
+        </WithClients>
+      )}
+    </WithUserSession>
+  );
+};
 
 export default View;
