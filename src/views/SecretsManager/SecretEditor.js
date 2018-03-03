4589e155dcdc51e95bbc938d49eb5b9ca6fba366
JQuinnie <jenn8866@gmail.com>
2018-03-03 17:12:04 -0600

    Remove back button as it does not reload properly in all scenarios

--- src/views/SecretsManager/SecretEditor.js
+++ src/views/SecretsManager/SecretEditor.js
@@ -287,9 +287,6 @@ export default class SecretEditor extends React.PureComponent {
           </ButtonToolbar>
         ) : (
           <ButtonToolbar>
-            <Button onClick={this.props.reloadSecrets}>
-              <Glyphicon glyph="chevron-left" /> Back to List
-            </Button>
             <Button bsStyle="success" onClick={this.startEditing}>
               <Glyphicon glyph="pencil" /> Edit Secret
             </Button>
