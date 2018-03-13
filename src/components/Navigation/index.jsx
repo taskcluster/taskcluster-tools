c4cb56329dc47a259c146b78b9ea3cca59919f01
Eli Perelman <eli@eliperelman.com>
2018-03-13 17:54:06 -0500

    Revert "Algolia docs search in navigation" (#500)
    * Revert "Add tutorial section from tc-docs to tools (#499)"
    
    This reverts commit ff3d8586e18b8d628d05f158747e033d94024b79.
    
    * Revert "Algolia docs search in navigation (#498)"
    
    This reverts commit cd57a4459bce9a28d3a5f8bc67ef793ee5541d33.

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -2,35 +2,12 @@ import { PureComponent } from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Link } from 'react-router-dom';
 import Icon from 'react-fontawesome';
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  MenuItem,
-  NavItem,
-  FormControl
-} from 'react-bootstrap';
-import docsearch from 'docsearch.js/dist/cdn/docsearch';
-import 'docsearch.js/dist/cdn/docsearch.css';
+import { Navbar, Nav, NavDropdown, MenuItem } from 'react-bootstrap';
 import CredentialsMenu from '../CredentialsMenu';
 import links from '../../links';
-import { navigation, docsSearchContainer } from './styles.module.css';
+import { navigation } from './styles.module.css';
 
 export default class Navigation extends PureComponent {
-  search = null;
-
-  componentDidMount() {
-    this.search = docsearch({
-      apiKey: process.env.ALGOLIA_API_KEY,
-      indexName: process.env.ALGOLIA_INDEX_NAME,
-      inputSelector: '#docs-search',
-      autocompleteOptions: {
-        autoselect: true,
-        openOnFocus: true
-      }
-    });
-  }
-
   render() {
     return (
       <div className={navigation}>
@@ -41,14 +18,6 @@ export default class Navigation extends PureComponent {
             </Navbar.Brand>
           </Navbar.Header>
           <Nav pullRight>
-            <NavItem className={docsSearchContainer}>
-              <FormControl
-                type="text"
-                id="docs-search"
-                placeholder="Search docs..."
-                bsSize="sm"
-              />
-            </NavItem>
             <NavDropdown key={1} title="Tools" id="tools">
               {links.map(
                 ({ title, link, icon }) =>
